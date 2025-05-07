e4ad0465e38bd44d09b5dd4f7011eff1988ad1cd
Andre <andre.nfx@gmail.com>
2025-05-06 21:18:21 -0300

    aa

--- backend/main.py
+++ backend/main.py
@@ -5,10 +5,11 @@ from fastapi import FastAPI
 app = FastAPI()
 
 # Pega a variável PORT ou usa 8000 como fallback
-port = int(os.environ.get("PORT", 8000))
-
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    import uvicorn
+
+    port = int(os.environ.get("PORT", 8000))  # usa 8000 se PORT não existir
+    uvicorn.run("backend.main:app", host="0.0.0.0", port=port, reload=False)
     
     
     
