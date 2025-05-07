b276884df5a0ac0f36063cd7e20843a19454a253
Andre <andre.nfx@gmail.com>
2025-05-06 21:39:19 -0300

    aa

--- backend/main.py
+++ backend/main.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     import uvicorn
 
     port = int(os.environ.get("PORT", 8000))  # usa 8000 se PORT não existir
-    uvicorn.run("backend.main:app", host="0.0.0.0", port=port, reload=False)
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
 
 # Rota WebSocket
 @app.websocket("/ws")
