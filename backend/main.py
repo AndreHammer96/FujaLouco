f4a22a5ee9bd0e7d43d004c1ad594c21320d41f1
Andre <andre.nfx@gmail.com>
2025-05-06 21:08:15 -0300

    aa

--- backend/main.py
+++ backend/main.py
@@ -9,3 +9,6 @@ port = int(os.environ.get("PORT", 8000))
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=port)
+    
+def read_root():
+    return {"message": "API funcionando!"}
