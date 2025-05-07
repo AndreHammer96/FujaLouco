e2677e232838230f5fb86d3be7eccf6bf72bb06e
Andre <andre.nfx@gmail.com>
2025-05-06 21:11:11 -0300

    aa

--- backend/main.py
+++ backend/main.py
@@ -10,5 +10,16 @@ port = int(os.environ.get("PORT", 8000))
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=port)
     
+    
+    
+@app.get("/", response_class=HTMLResponse)
 def read_root():
-    return {"message": "API funcionando!"}
+    return """
+    <html>
+        <head><title>Fuja Louco</title></head>
+        <body>
+            <h1>API funcionando!</h1>
+            <p>Se você esperava um site aqui, precisa servir o front-end estático.</p>
+        </body>
+    </html>
+    """
